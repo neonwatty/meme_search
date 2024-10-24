3a49f674959acf5c4b6da2afebb1a2a91f400cf7
Jeremy Watt <jermwatt@gmail.com>
2024-10-24 09:33:20 -0700

    working on nested tags build

--- rails/app/controllers/image_cores_controller.rb
+++ rails/app/controllers/image_cores_controller.rb
@@ -39,6 +39,7 @@ class ImageCoresController < ApplicationController
 
   # PATCH/PUT /image_cores/1 or /image_cores/1.json
   def update
+    puts "params --> #{params}"
     puts "image_update_params --> #{image_update_params}"
     respond_to do |format|
       if @image_core.update(image_update_params)
@@ -73,20 +74,36 @@ class ImageCoresController < ApplicationController
     end
 
     def image_update_params
-      params.require(:image_core).permit(:description, :image_tags)
-      if params[:image_core][:image_tags].present?
-        test = params[:image_core][:image_tags].split(",")
-        puts "test -->#{test}"
-        test = test.map {|tag| {name: tag}}
-        puts "test -->#{test}"
-
-
-
-        params[:image_core][:image_tags_attributes] = test
-        puts params
-        params[:image_core].delete(:image_tags)  # Remove the old key
+      permitted_params = params.require(:image_core).permit(:description, image_tags_attributes: [:name, :_destroy])
+      
+      # Convert names TagName ids
+      puts "permitted_params --> #{permitted_params}"
+      if permitted_params[:image_tags_attributes].present?
+        puts "hi"
+        tag_names = permitted_params[:image_tags_attributes].values.map {|item| item[:name]}
+        tag_name_ids = tag_names.map {|name| TagName.find_by({name: name})}#.map {|result| result.id}
+        permitted_params.delete(:image_tags_attributes)
+        tag_name_ids_hash = tag_name_ids.map {|key| {tag_name: key}}
+        permitted_params[:image_tags_attributes] = tag_name_ids_hash
       end
-      return params
+
+      permitted_params
+
+
+      # params.require(:image_core).permit(:description, :image_tags)
+      # if params[:image_core][:image_tags].present?
+      #   test = params[:image_core][:image_tags].split(",")
+      #   puts "test -->#{test}"
+      #   test = test.map {|tag| {name: tag}}
+      #   puts "test -->#{test}"
+
+
+
+      #   params[:image_core][:image_tags_attributes] = test
+      #   puts params
+      #   params[:image_core].delete(:image_tags)  # Remove the old key
+      # end
+      # return params
       # params.require(:image_core).permit(:description, image_tags_attributes: [:name])  
 
     end
