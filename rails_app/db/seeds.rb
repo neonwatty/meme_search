9f16f0060c696e81b15f2cdc4c164472cc989370
Jeremy Watt <jermwatt@gmail.com>
2024-11-04 10:01:22 -0700

    prepping integration tests

--- rails_app/db/seeds.rb
+++ rails_app/db/seeds.rb
@@ -8,6 +8,16 @@ if File.directory?(example_dir)
   examples_path.save!
 end
 
+# create image_path for memes in example_memes_2 directory
+base_dir = Dir.getwd
+example_memes_subdir =  "example_memes_2"
+example_dir = base_dir + "/public/memes/" +  example_memes_subdir
+puts example_dir
+if File.directory?(example_dir)
+  examples_path = ImagePath.new({ name: example_memes_subdir })
+  examples_path.save!
+end
+
 # create two tags
 my_tag_name = "one"
 tag_one = TagName.new({ name: my_tag_name, color: "black" })
@@ -18,8 +28,14 @@ tag_two = TagName.new({ name: my_tag_name, color: "red" })
 tag_two.save!
 
 # tag a few images with these tags
-first_example_meme = ImageCore.first
-first_example_meme.update({ image_tags_attributes: [ { tag_name: tag_one } ] })
+current_imgs = ImageCore.order(created_at: :desc)
+first_meme = current_imgs[0]
+second_meme = current_imgs[1]
+third_meme = current_imgs[2]
+fourth_meme = current_imgs[3]
 
-second_example_meme = ImageCore.last
-second_example_meme.update({ image_tags_attributes: [ { tag_name: tag_one }, { tag_name: tag_two } ] })
+first_meme.update({ image_tags_attributes: [ { tag_name: tag_one } ] })
+second_meme.update({ image_tags_attributes: [ { tag_name: tag_one }, { tag_name: tag_two } ] })
+third_meme.update({ image_tags_attributes: [ { tag_name: tag_two } ] })
+
+
