29ce78658c3843b8bf5798453113c5a88b7f7ffd
Jeremy Watt <jermwatt@gmail.com>
2024-11-04 14:49:04 -0700

    integration tests

--- rails_app/test/integration/tag_name_test.rb
+++ rails_app/test/integration/tag_name_test.rb
@@ -1,4 +1,5 @@
 require "test_helper"
+require "capybara/minitest"
 
 class TagNameTest < ActionDispatch::IntegrationTest
   setup do
@@ -13,10 +14,17 @@ class TagNameTest < ActionDispatch::IntegrationTest
     # confirm settings li exists in navbar and that Tags exist underneath it
     assert_select "ul#navigation" do
       assert_select "li#settings" do 
-        assert_select "#dropdown_tags"
-        
-        # redirect to tag_names settings page
-        assert_current_path [:settings, :tag_names]
+        assert_select "#dropdown_tags" do
+          begin
+            puts "INFO A"
+            assert_select "h1", "Current tags"
+            puts "INFO B"
+          rescue Minitest::Assertion => e
+            puts "Assertion failed: #{e.message}"
+            puts page.html  # Print the current HTML of the page
+            raise e  # Re-raise the exception to fail the test
+          end
+        end
       end
     end
   end
