e675058bd2ea872f7e5582eeb7a97c06cd3e886d
Jeremy Watt <jermwatt@gmail.com>
2024-11-06 11:43:06 -0700

    system tests

--- rails_app/test/system/index_filter_test.rb
+++ rails_app/test/system/index_filter_test.rb
@@ -1,22 +1,16 @@
 require "application_system_test_case"
 
 class IndexFilterTest < ApplicationSystemTestCase
-  test "visit root and check all results visible" do
-    # visit root path
-    visit root_path
-
-    # count results
-    first_meme_count = all("div[id^='image_core_card_']").count
-    assert first_meme_count == 4
-  end
-
-  test "visit root, open filters, escape" do
+  setup do
     # visit root
     visit root_path
 
-    # count memes
-    first_meme_count = all("div[id^='image_core_card_']").count
+    # initial count memes
+    @first_meme_count = all("div[id^='image_core_card_']").count
+    assert @first_meme_count == 4
+  end
 
+  test "visit root, open filters, escape" do
     # confirm slider is not visible
     assert_selector "div#filters_slideover", visible: false
 
@@ -34,16 +28,10 @@ class IndexFilterTest < ApplicationSystemTestCase
 
     # re-count memes and confirm count
     second_meme_count = all("div[id^='image_core_card_']").count
-    assert second_meme_count == first_meme_count
+    assert second_meme_count == @first_meme_count
   end
 
   test "visit root, open filters, click 'Close without filtering' button, return and count" do
-    # visit root
-    visit root_path
-
-    # count memes
-    first_meme_count = all("div[id^='image_core_card_']").count
-
     # confirm slider is not visible
     assert_selector "div#filters_slideover", visible: false
 
@@ -61,16 +49,10 @@ class IndexFilterTest < ApplicationSystemTestCase
 
     # confirm meme count unchanged
     second_meme_count = all("div[id^='image_core_card_']").count
-    assert second_meme_count == first_meme_count
+    assert second_meme_count == @first_meme_count
   end
 
-  test "visit root, open filters, apply tag configuration, return and count" do
-    # visit root
-    visit root_path
-
-    # count memes
-    first_meme_count = all("div[id^='image_core_card_']").count
-
+  test "visit root, open filters, check all tags, return and verify changes" do
     # confirm slider is not visible
     assert_selector "div#filters_slideover", visible: false
 
@@ -91,12 +73,71 @@ class IndexFilterTest < ApplicationSystemTestCase
     # apply filters
     click_on "Apply filters"
 
-    # count memes
+    # count memes - should have minus 1 from start
     sleep(0.2)
     second_meme_count = all("div[id^='image_core_card_']").count
-    puts "first_meme_count --> #{first_meme_count}"
-    puts "second_meme_count --> #{second_meme_count}"
+    assert second_meme_count == @first_meme_count - 1
+  end
 
-    assert_selector "farts"
+  test "visit root, open filters, check one tag, return and verify changes" do
+    # click on "open filters"
+    click_on "Open filters"
+
+    # confirm slideover id visible
+    assert_selector "div#filters_slideover", visible: true
+
+    # select tag one
+    assert_selector "div#tag_toggle", visible: true
+    find("#tag_toggle").click
+    find("#tag_0").check
+    find("#tag_toggle").click
+    find("#has_embeddings_checkbox").uncheck
+
+    # apply filters
+    click_on "Apply filters"
+
+    # count memes - should have minus 2 from start
+    sleep(0.2)
+    second_meme_count = all("div[id^='image_core_card_']").count
+    assert second_meme_count == @first_meme_count - 2
+  end
+
+  test "visit root, open filters, (keep) embeddings checked, return and verify changes" do
+    # click on "open filters"
+    click_on "Open filters"
+
+    # confirm slideover id visible
+    assert_selector "div#filters_slideover", visible: true
+
+    # apply filters
+    click_on "Apply filters"
+
+    # count memes - no memes have embeddings on init
+    sleep(0.2)
+    second_meme_count = all("div[id^='image_core_card_']").count
+    assert second_meme_count == 0
+  end
+
+  test "visit root, open filters, check one directory path, return and verify changes" do
+    # click on "open filters"
+    click_on "Open filters"
+
+    # confirm slideover id visible
+    assert_selector "div#filters_slideover", visible: true
+
+    # select path one
+    assert_selector "div#path_toggle", visible: true
+    find("#path_toggle").click
+    find("#path_1").uncheck
+    find("#path_toggle").click
+    find("#has_embeddings_checkbox").uncheck
+
+    # apply filters
+    click_on "Apply filters"
+
+    # count memes - no memes have embeddings on init
+    sleep(0.2)
+    second_meme_count = all("div[id^='image_core_card_']").count
+    assert second_meme_count == @first_meme_count - 2
   end
 end
