f58da0c1271b8785d029f201e46d47d5c0f5ed14
Jeremy Watt <jermwatt@gmail.com>
2024-11-16 06:55:26 -0700

    cleanup

--- meme_search/meme_search_app/__init__.py
+++
@@ -1,8 +0,0 @@
-import os
-
-base_dir = os.path.dirname(os.path.abspath(__file__))
-meme_search_root_dir = os.path.dirname(base_dir)
-abs_dir = "."
-
-vector_db_path = meme_search_root_dir + "/data/dbs/memes.faiss"
-sqlite_db_path = meme_search_root_dir + "/data/dbs/memes.db"
--- meme_search/tests/__init__.py
+++
@@ -1,7 +0,0 @@
-import os
-
-cwd = os.getcwd()
-base_test_dir = os.path.dirname(os.path.abspath(__file__))
-
-CONTAINER_NAME = "meme_search"
-STREAMLIT_APP_FILE = "meme_search_app/app.py"
